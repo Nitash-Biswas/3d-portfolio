271ebcff8386feb4e9748fe4dda4a38da17b5c50
Nitash-Biswas <nitash.nb@gmail.com>
2023-07-08 20:20:19 +0530

    Ok

--- 3d.js
+++ 3d.js
@@ -2,6 +2,7 @@ import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 
 // Scene, camera, renderer
 const scene = new THREE.Scene();
@@ -17,16 +18,23 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 // Adjust the camera position
-camera.position.set(-25, -25, 25);
-camera.rotateOnAxisY=90;
+// camera.position.set(-25, -25, 25);
+// camera.rotateOnAxisY=90;
 
 
 
 // Lighting
-const pointlight = new THREE.PointLight(0x000000);
-pointlight.position.set(1, 1, 1);
-const ambientlight = new THREE.AmbientLight(0xC2B775);
-scene.add(pointlight, ambientlight);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2);
+directionalLight.position.set(1, 1, 1); // Set the position of the light source
+
+const ambientlight = new THREE.AmbientLight(0xffffff,1);
+scene.add(ambientlight,directionalLight);
+
+// Create a PointLightHelper and pass the PointLight as an argument
+const lightHelper = new THREE.PointLightHelper(ambientlight, 1);
+
+// Add the lightHelper to the scene
+scene.add(lightHelper);
 
 // Orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -40,17 +48,24 @@ controls.enableRotate = false; // Disable rotation control
 
 let object;
 const loader = new GLTFLoader();
+
+//Draco Loader
+const dLoader = new DRACOLoader();
+dLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.6/');
+dLoader.setDecoderConfig({type: 'js'});
+loader.setDRACOLoader(dLoader);
+
 loader.load(
-  'art/home.gltf',
+  'art/wizard.gltf',
   (gltf) => {
     object = gltf.scene;
-    let scale = 1;
+    let scale = 0.4;
     object.scale.set(scale, scale, scale);
 
     
 
     scene.add(object);
-    object.position.set(-10, -2,10);
+    object.position.set(-10, 0,1);
     camera.lookAt(object.position); // Look at the ball's position initially 
   },
   (xhr) => {
@@ -61,26 +76,27 @@ loader.load(
   }
 );
 
-// let secobject;
-// loader.load(
-//   'art/home.gltf',
-//   (gltf) => {
-//     secobject = gltf.scene;
-//     let scale = 2.5;
-//     secobject.scale.set(scale, scale, scale);
+let secobject;
+loader.load(
+  'art/home.gltf',
+  (gltf) => {
+    secobject = gltf.scene;
+    let scale = 4.5;
+    secobject.scale.set(scale, scale, scale);
 
     
 
-//     scene.add(secobject);
-//     secobject.position.set(-10, -2,10);
-//   },
-//   (xhr) => {
-//     console.log(`${(xhr.loaded / xhr.total) * 100}% loaded`);
-//   },
-//   (error) => {
-//     console.error('Error loading GLTF object:', error);
-//   }
-// );
+    scene.add(secobject);
+    secobject.position.set(-10, 0,10);
+    secobject.rotation.y = Math.PI/2;
+  },
+  (xhr) => {
+    console.log(`${(xhr.loaded / xhr.total) * 100}% loaded`);
+  },
+  (error) => {
+    console.error('Error loading GLTF object:', error);
+  }
+);
 
 
 // Grid
@@ -97,8 +113,8 @@ document.addEventListener('keyup', (event) => {
 });
 
 // Camera settings
-const cameraDistance = 30; // Distance from the ball
-const cameraHeight = 30; // Height above the ball
+const cameraDistance = 40; // Distance from the ball
+const cameraHeight = 35; // Height above the ball
 const cameraFollowSpeed = 0.03; // Speed of camera following (lower value for smoother effect)
 
 camera.position.set(cameraDistance, cameraHeight, cameraDistance);
@@ -109,54 +125,60 @@ let targetCameraPosition = new THREE.Vector3();
 let cameraVelocity = new THREE.Vector3();
 
 
-const rotationSpeed = 0.01; // Adjust the rotation speed as needed
-
-function rotateObject() {
+function levitateObject() {
   if(object){
-    if (!keyboard['KeyW'] && !keyboard['KeyS'] && !keyboard['KeyA'] && !keyboard['KeyD']) {
-      object.rotation.y += rotationSpeed;
+    
+      const oscillationSpeed = 0.005; 
+      const amplitude = 0.3; 
+
+    object.position.y = Math.sin(Date.now() * oscillationSpeed) * amplitude;
+      
     }
   }
-  
-}
-
-
-  
 
 
 function moveobject() {
-  
-
-  if(object){
+  if (object) {
     const speed = 0.1;
-  if (keyboard['KeyW']) {
-    object.position.z -= speed;
-    object.position.x -= speed;
-  }
-  if (keyboard['KeyS']) {
-    object.position.z += speed;
-    object.position.x += speed;
-  }
-  if (keyboard['KeyA']) {
-    object.position.x -= speed;
-    object.position.z += speed;
-  }
-  if (keyboard['KeyD']) {
-    object.position.x += speed;
-    object.position.z -= speed;
-  }
 
-  // Update target camera position relative to the ball
-  targetCameraPosition.x = object.position.x + cameraDistance;
-  targetCameraPosition.y = object.position.y + cameraHeight;
-  targetCameraPosition.z = object.position.z + cameraDistance;
+    if (keyboard['KeyW']) {
+      object.position.x -= speed;
+      object.rotation.y = 0; 
+    }
 
-  // Smoothly move the camera towards the target position
-  cameraVelocity.lerp(targetCameraPosition, cameraFollowSpeed);
-  camera.position.copy(cameraVelocity);
+    if (keyboard['KeyS']) {
+      object.position.x += speed;
+      object.rotation.y = Math.PI; 
+    }
+
+    if (keyboard['KeyA']) {
+      object.position.z += speed;
+      object.rotation.y = Math.PI / 2; 
+      
+    }
+
+    if (keyboard['KeyD']) {
+      object.position.z -= speed; 
+      object.rotation.y = -Math.PI / 2; 
+    }
+
+    // Update target camera position relative to the ball
+    targetCameraPosition.x = object.position.x + cameraDistance;
+    targetCameraPosition.y = object.position.y + cameraHeight;
+    targetCameraPosition.z = object.position.z + cameraDistance;
+
+    // Smoothly move the camera towards the target position
+    cameraVelocity.lerp(targetCameraPosition, cameraFollowSpeed);
+    camera.position.copy(cameraVelocity);
   }
-  
 }
+
+
+
+
+  
+
+
 
 
 
@@ -164,7 +186,7 @@ function animate() {
   requestAnimationFrame(animate);
 
   moveobject();
-  rotateObject();
+  levitateObject();
 
   // Render the scene
   renderer.render(scene, camera);
